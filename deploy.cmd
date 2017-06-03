faf8e996aa6a80415e6d1e504d99b0f55403a737
Daniel O'Leary <danieloleary89@gmail.com>
2017-06-03 18:32:52 +0100

    updated deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,12 +1,16 @@
-@echo off
+
 .paket\paket.bootstrapper.exe
 if errorlevel 1 (
   exit /b %errorlevel%
 )
 
+@echo "restoring paket dependencies"
 .paket\paket.exe restore
 if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-packages\FAKE\tools\FAKE.exe deploy.fsx
+REM packages\FAKE\tools\FAKE.exe deploy.fsx
+
+@echo "Copying files to web root"
+xcopy /s /y . d:\home\site\wwwroot\
