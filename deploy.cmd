7007fcee05e2235487c58268787539e3d3ac0e92
Daniel O'Leary <danieloleary89@gmail.com>
2017-06-03 22:31:49 +0100

    deploy change

--- deploy.cmd
+++ deploy.cmd
@@ -1,16 +1,43 @@
-@echo off
+@if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
+setlocal enabledelayedexpansion
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Deployment
+:: ----------
+
+:: Restore NuGet packages
 .paket\paket.bootstrapper.exe
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-if not exist paket.lock (
-  .paket\paket.exe install
-) else (
-  .paket\paket.exe restore
-)
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
+.paket\paket.exe restore
 
+:: Start main build script
+packages\FAKE\tools\FAKE.exe deploy.fsx
 
-packages\FAKE\tools\FAKE.exe %* --fsiargs deploy.fsx
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+IF !ERRORLEVEL! NEQ 0 goto error
+
+goto end
+
+:: Execute command routine that will echo out when error
+:ExecuteCmd
+setlocal
+set _CMD_=%*
+call %_CMD_%
+if "%ERRORLEVEL%" NEQ "0" echo Failed exitCode=%ERRORLEVEL%, command=%_CMD_%
+exit /b %ERRORLEVEL%
+
+:error
+endlocal
+echo An error has occurred during web site deployment.
+call :exitSetErrorLevel
+call :exitFromFunction 2>nul
+
+:exitSetErrorLevel
+exit /b 1
+
+:exitFromFunction
+()
+
+:end
+endlocal
+echo Finished successfully.
