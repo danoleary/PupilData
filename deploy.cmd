d4474c23c33803275ea46e6281e00d335eaad371
Daniel O'Leary <danieloleary89@gmail.com>
2017-06-03 19:57:51 +0100

    updated deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,10 +1,15 @@
+@echo off
+cls
 
-.paket\paket.bootstrapper.exe
+mkdir .paket
+REM TODO - might want to do an IF EXISTS *or* a SHA check
+curl https://github.com/fsprojects/Paket/releases/download/1.2.0/paket.bootstrapper.exe -L --insecure -o .paket\paket.bootstrapper.exe
+
+.paket\paket.bootstrapper.exe prerelease
 if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-@echo "restoring paket dependencies"
 .paket\paket.exe restore
 if errorlevel 1 (
   exit /b %errorlevel%
